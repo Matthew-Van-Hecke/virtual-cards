c2f3e253c2b6372e8f146ef4535d5498db5e94a4
Matthew VH <vanheckematt@gmail.com>
2020-09-27 18:25:15 -0500

    Installed ejs and added a views folder with home.ejs in it.

--- app.js
+++ app.js
@@ -2,4 +2,12 @@ const express = require("express");
 
 const app = express();
 
-app.listen(3000);
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.listen(3000, () => {
+    console.log("---SERVER IS RUNNING---");
+});
